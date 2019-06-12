06d635e44dee9d2da1d31174c66794465e8538c8
Katarina Sekula <katarina.sekula@omega-software.hr>
2019-06-12 13:27:04 +0200

    Save telemetry test

---
+++ gatling-charts-highcharts-bundle-3.0.3/user-files/simulations/KafkaSaveTelemetrySimulation.scala
@@ -0,0 +1,33 @@
+import io.gatling.core.Predef._
+import org.apache.kafka.clients.producer.ProducerConfig
+import scala.concurrent.duration._
+
+import com.github.mnogu.gatling.kafka.Predef._
+
+class KafkaSaveTelemetrySimulation extends Simulation {
+  val kafkaConf = kafka
+    .topic("save-telemetry")
+    .properties(
+      Map(
+        ProducerConfig.ACKS_CONFIG -> "1",
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "http://192.168.113.134:29092",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.StringSerializer",
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.StringSerializer"))
+
+  val scn = scenario("Kafka Test")
+    .feed(jsonFile("saveTelemetry.json").circular)
+    .exec(kafka("request").send[String](
+      "{" + 
+        "\"messageAsJson\": ${messageAsJson}," +
+        "\"deviceId\": ${deviceId}," +
+        "\"applicationId\": ${applicationId}," +
+        "\"assetUnitIds\": ${assetUnitIds}," +
+      "}"))
+
+  setUp(
+    scn
+      .inject(rampUsersPerSec(5000) to 10000 during (10 seconds)))//.inject(constantUsersPerSec(10) during(10 seconds))) 
+    .protocols(kafkaConf)
+}
